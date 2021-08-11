4cba7a9221963ed10aed87ea23f499693fd172e6
Alex Muhortov <44553240+MuhortovAA@users.noreply.github.com>
2021-08-11 10:26:56 +0300

    add

---
+++ CBTEventPlaningApp/CBTEventPlaningApp/Controllers/AccountController.cs
@@ -0,0 +1,142 @@
+﻿using CBTEventPlaningApp.Services.Email;
+using CBTEventPlaningApp.Services.Models;
+using CBTEventPlaningApp.Services.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CBTEventPlaningApp.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+        public AccountController(UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signInMgr)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl) => View(new LoginModel { ReturnUrl = returnUrl });
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    var result = await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        string strUrl = loginModel?.ReturnUrl ?? "/";
+                        return Redirect(strUrl);
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Неверное имя пользователя или пароль");
+            return View(loginModel);
+        }
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+
+        [AllowAnonymous]
+        public IActionResult Create() => View();
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = new IdentityUser { UserName = model.Name, Email = model.Email, PhoneNumber = model.PhoneNumber };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    TempData["message"] = $"Пользователь: {user.UserName} успешно создан.";
+                    return RedirectToAction(actionName: "Index", controllerName: "Home");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email };
+                // добавляем пользователя
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // генерация токена для пользователя
+                    var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action(
+                        "ConfirmEmail",
+                        "Account",
+                        new { userId = user.Id, code = code },
+                        protocol: HttpContext.Request.Scheme);
+                    EmailService emailService = new EmailService();
+                    await emailService.SendEmailAsync(model.Email, "Confirm your account",
+                        $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+
+                    return Content("Для завершения регистрации проверьте электронную почту и перейдите по ссылке, указанной в письме");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return View("Error");
+            }
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var result = await userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Home");
+            else
+                return View("Error");
+        }
+    }
+}
---
+++ CBTEventPlaningApp/CBTEventPlaningApp/Controllers/EventController.cs
@@ -0,0 +1,72 @@
+﻿using CBTEventPlaningApp.Services.Interface;
+using CBTEventPlaningApp.Services.Models;
+using CBTEventPlaningApp.Services.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CBTEventPlaningApp.Controllers
+{
+    public class EventController : Controller
+    {
+        private IEFRepository repository;
+
+        public EventController(IEFRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Detailes(string id) => View(repository.GetAllInfoEvent(id));
+        public IActionResult Create() => View(new CreateEvent());
+
+        [HttpPost]
+        public IActionResult Create(CreateEvent newevent)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int id = repository.SaveEvent(newevent);
+                TempData["message"] = $"Создано событие.";
+                return RedirectToAction(actionName: "Detailes", controllerName: "Event", routeValues: new { id = id.ToString() });
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult AddTitle(string Id) => View(new CBTDetailes() { EventId = int.Parse(Id), Title = "название", SubTitle = "описание" });
+        [HttpPost]
+        public IActionResult AddTitle(CBTDetailes detail)
+        {
+
+            if (ModelState.IsValid)
+            {
+                repository.SaveTitle(detail);
+                TempData["message"] = $"Добавлено.";
+                return RedirectToAction(actionName: "Detailes", controllerName: "Event", routeValues: new { id = detail.EventId.ToString() });
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult EditTitle(string Id) => View(repository.GetDetaileById(Id));
+        [HttpPost]
+        public IActionResult EditTitle(CBTDetailes detail)
+        {
+
+            if (ModelState.IsValid)
+            {
+                repository.EditTitle(detail);
+
+                TempData["message"] = $"Обновлено.";
+                return RedirectToAction(actionName: "Detailes", controllerName: "Event", routeValues: new { id = detail.EventId.ToString() });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
